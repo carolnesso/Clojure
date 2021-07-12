863fbf384c40747f1e321eea9de9c1663fae7ac9
Carolnesso <acng@ic.ufal.br>
2021-07-12 16:59:20 -0300

    Autualiza core de aula

--- aula/src/aula/core.clj
+++ aula/src/aula/core.clj
@@ -1,16 +1,38 @@
 (ns aula.core
   (:gen-class))
 
+; Software bancário 
+
+(defn criar-transacao
+  [nome valor]
+  {:nome nome :valor valor})
+
 (def transacoes
-  [{:nome "venda" :valor 12.99}
-   {:nome "compra" :valor -100.}
-   {:nome "venda" :valor 123.45}
-   {:nome "compra" :valor -1.99}
+  [
+   (criar-transacao "venda" 12.99)
+   (criar-transacao "venda" 123.45)
+   (criar-transacao "compra" -100)
+   (criar-transacao "compra" -1.99)
    ])
+
+(defn get-valores-a
+  [transacoes-real]
+  (map :valor transacoes-real))
+
+(defn get-valores-b
+  [transacoes-real]
+  (map (fn[x] (:valor x)) transacoes-real))
+
+(defn get-valores-c
+  [transacoes-real]
+  (map #(:valor %) transacoes-real))
+
+; map == for it = "o que ele vai executar" "base de dados"
 
 (defn -main
   [& args]
   (println "Hello, World!"))
 
- ; defn define funções
- ;ja def define variaveis 
+; defn define funções
+; def define variaveis 
+; nil == NULL == none
